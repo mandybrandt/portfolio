de5875fb473a0754c3d5c19e6f10cfb813519eb1
Amanda Brandt <106720199+mandybrandt@users.noreply.github.com>
2024-03-15 20:44:09 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+//   function filterFunction() {
+//     var input, filter, ul, li, a, i;
+//     input = document.getElementById("myInput");
+//     filter = input.value.toUpperCase();
+//     div = document.getElementById("myDropdown");
+//     a = div.getElementsByTagName("a");
+//     for (i = 0; i < a.length; i++) {
+//       txtValue = a[i].textContent || a[i].innerText;
+//       if (txtValue.toUpperCase().indexOf(filter) > -1) {
+//         a[i].style.display = "";
+//       } else {
+//         a[i].style.display = "none";
+//       }
+//     }
+//   }
+
+// function mtSearch(mountain){
+//     mountain = letter.toLowerCase();
+//     if(mt. rainier) {
+//         return href = "https://www.alltrails.com/trail/us/washington/mount-rainier-standard-summit-route";
+
+//     }
+//     return "No information for that mountain."
+    
+// }
+
+// document.getElementById("mountainSearch") {
+//     var mountain = document.getElementById
+// }
